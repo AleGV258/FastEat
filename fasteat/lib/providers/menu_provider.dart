55ac13c6eb74931383019cf12cff080bfffdc587
MichellGV <ale.gv258@gmail.com>
2021-10-26 22:33:42 -0500

    Favorito

--- fasteat/lib/providers/menu_provider.dart
+++ fasteat/lib/providers/menu_provider.dart
@@ -3,32 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 //esta clase es para recuperar los datos del JSON de categorias (creo que estos datos se deberian recuperar de la base de datos no de un JSON)
-class _GetICategorias {
+class _GetFavoritos {
   List<dynamic> opciones = [];
-  _GetICategorias() {
+  _GetFavoritos() {
     cargarData();
   }
   Future<List<dynamic>> cargarData() async {
-    final resp = await rootBundle.loadString('data/categorias.json');
+    final resp = await rootBundle.loadString('data/favoritos.json');
     Map dataMap = json.decode(resp);
     opciones = dataMap['rutas'];
     return opciones;
   }
 }
-final categorias = new _GetICategorias(); //la convierte a publica la clase
-
-
-//esta clase es para recuperar los datos del JSON de rutas
-class _MenuProvider {
-  List<dynamic> opciones = [];
-  _MenuProvider() {
-    cargarData();
-  }
-  Future<List<dynamic>> cargarData() async {
-    final respuesta = await rootBundle.loadString('data/rutas_opts.json');
-    Map dataMap = json.decode(respuesta);
-    opciones = dataMap['rutas'];
-    return opciones;
-  }
-}
-final menuProvider = new _MenuProvider();
+final fav = new _GetFavoritos(); //la convierte a publica la clase
